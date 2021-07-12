ef6a96eaa9da8e668f2698bf62b4fe1d0253798b
VitoPalmisano <61830892+VitoPalmisano@users.noreply.github.com>
2021-07-12 22:43:58 +0200

    Add files via upload

---
+++ modules/__init__.py
@@ -0,0 +1,5 @@
+from .deeplab import DeeplabV3
+from .residual import IdentityResidualBlock, ResidualBlock
+from .misc import GlobalAvgPool2d
+from .build_BiSeNet import *
+from .build_contextpath import build_contextpath
